a60bd6677a97f5c2e10625f82d3bc84236446494
Remidelgutte <delgutte.remi@hotmail.com>
2024-11-09 14:16:18 +0100

    ajout input

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ console.log(solde + "€" + " est le nouveau solde après le dépôt.");
 if (solde > retrait) {
   solde = solde - retrait;
   historiqueOperations.push("Retrait de " + retrait + "€");
-  console.log("Vous avez reiré " + retrait + "€." + " Nouveau solde : " + solde + "€");
+  console.log("Vous avez retiré " + retrait + "€." + " Nouveau solde : " + solde + "€");
 } else {
   console.log("Soldes insuffisant pour effectuer le retrait.");
 }
@@ -21,18 +21,19 @@ if (solde > retrait) {
 solde = solde * (1 + tauxInteret);
 console.log("Intérêts annuels de 3% ajoutés. Nouveau solde : " + solde + "€");
 
-solde = solde = solde + nouveauDepot;
+solde = solde + nouveauDepot;
 historiqueOperations.push("Dépôt de " + nouveauDepot + "€");
-console.log(solde + "€" + "est le nouveau solde après le dépôt;");
+console.log(solde + "€" + " est le nouveau solde après le dépôt;");
 
 if (solde > nouveauRetrait) {
   solde = solde - nouveauRetrait;
   historiqueOperations.push("Retrait de " + nouveauRetrait + "€");
-  console.log("Vous avez reiré " + nouveauRetrait + "€." + " Nouveau solde : " + solde + "€");
+  console.log("Vous avez retiré " + nouveauRetrait + "€." + " Nouveau solde : " + solde + "€");
 } else {
   console.log("Soldes insuffisant pour effectuer le retrait.");
 }
 
 solde = solde * (1 + tauxInteret);
 console.log("Intérêts annuels de 3% ajoutés. Nouveau solde : " + solde + "€");
+
 console.log(historiqueOperations);
